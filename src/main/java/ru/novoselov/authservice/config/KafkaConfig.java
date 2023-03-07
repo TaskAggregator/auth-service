4a71e879c2d3e17c69e58ae01effdc035a145703
vadimnovoselov <i.am.vadimnovoselov@gmail.com>
2023-03-07 18:08:15 +0300

    Add BOOTSTRAP_SERVERS_CONFIG property to kafkaAdmin bean

--- src/main/java/ru/novoselov/authservice/config/KafkaConfig.java
+++ src/main/java/ru/novoselov/authservice/config/KafkaConfig.java
@@ -1,13 +1,29 @@
 package ru.novoselov.authservice.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 import ru.novoselov.authservice.properties.KafkaTopics;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String kafkaUrl;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaUrl);
+        return new KafkaAdmin(configs);
+    }
     @Bean
     public NewTopic userSignupTopic() {
         return TopicBuilder
--- src/main/java/ru/novoselov/authservice/model/request/SignupRequest.java
+++ src/main/java/ru/novoselov/authservice/model/request/SignupRequest.java
@@ -1,6 +1,10 @@
 package ru.novoselov.authservice.model.request;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
